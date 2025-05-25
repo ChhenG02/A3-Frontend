0d00b872ae9d0a068a34bba0bc3e04a4e87ab182
ChhenG02 <itcdev@edu.kh>
2025-05-26 00:21:07 +0700

    fixed conflict

--- src/app/resources/r3-admin/admin.routes.ts
+++ src/app/resources/r3-admin/admin.routes.ts
@@ -1,20 +1,12 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './a1-dashboard/component';
 
-
 import { SaleComponent } from './a2-sale/component';
 
 import { UserComponent } from './a5-user/u1-listing/component';
 import { ProductComponent } from './a3-product/component';
 import { SetupLayoutComponent } from './a6-setup/component';
 import { ProductTypeComponent } from './a6-setup/s1-type/component';
-
-import { ProductTypeComponent } from "./a3-product/p2-type/component";
-import { SaleComponent } from "./a2-sale/component";
-import { UserComponent } from "./a5-user/u1-listing/component";
-import { ProductComponent } from "./a3-product/p1-product/component";
-import { ViewUserComponent } from "./a6-setting/u2-view/component";
-
 
 export default [
     {
@@ -27,25 +19,7 @@ export default [
     },
     {
         path: 'product',
-
         component: ProductComponent,
-
-        children: [
-            {
-                path: 'all',
-                component: ProductComponent
-            },
-        ]
-    },
-    {
-        path: 'stock',
-        children: [
-            {
-                path: 'all',
-                component: ProductComponent
-            },
-        ]
-
     },
 
     //  {
@@ -68,10 +42,4 @@ export default [
             },
         ],
     },
-
-    {
-        path: 'setting',
-        component: ViewUserComponent
-    },
-
 ] as Routes;
