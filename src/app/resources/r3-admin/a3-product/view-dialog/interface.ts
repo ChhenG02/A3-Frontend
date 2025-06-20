69fbf6afb534a75cd954fb3a606ab983f5af2267
Chea Oudompanhariddh <cheaodompanhariddh.com>
2025-06-20 14:40:58 +0700

    Add promotion condition for view-dialog

--- src/app/resources/r3-admin/a3-product/view-dialog/interface.ts
+++ src/app/resources/r3-admin/a3-product/view-dialog/interface.ts
@@ -15,4 +15,7 @@ export interface Data {
     total_price: number,
     ordered_at?: Date,
     cashier: { id: number, name: string },
+    discount?: number;
+    unit_price: number;
+    promotion_id?: number;
 }
